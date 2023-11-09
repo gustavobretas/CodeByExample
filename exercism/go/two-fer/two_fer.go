e6a7e765995005db2d755c8ceef1c2faf0531830
Gustavo Bretas <gustavobretas@gmail.com>
2023-11-09 15:19:18 -0400

    exercism GO Two Fer

---
+++ exercism/go/two-fer/two_fer.go
@@ -0,0 +1,11 @@
+package twofer
+
+import "fmt"
+
+// ShareWith is about share with a given name
+func ShareWith(name string) string {
+	if len(name) == 0 {
+		name = "you"
+	}
+	return fmt.Sprintf("One for %s, one for me.", name)
+}
