fa9ca30ee8f1ccc850c0a2e84e95ec5a78bc8299
Gustavo Bretas <gustavobretas@gmail.com>
2023-11-14 16:17:02 -0400

    Nextjs.org Learn - Chapter 10 Partial Prerendering (Optional)

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -1,9 +1,9 @@
-import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import { lusitana } from "@/app/ui/fonts";
-import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
-import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
 import { Suspense } from "react";
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from "@/app/ui/skeletons";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
     
