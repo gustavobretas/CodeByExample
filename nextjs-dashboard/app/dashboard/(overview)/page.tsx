210598ba1804ecdcd4f7e17dbfadd075826e5e07
Gustavo Bretas <gustavobretas@gmail.com>
2023-11-18 16:02:57 -0400

    Nextjs.org Learn - Extra Patterning Breadcrumb

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -5,6 +5,7 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import CardWrapper from "@/app/ui/dashboard/cards";
 import { Metadata } from 'next';
+import Breadcrumbs from "@/app/ui/common/breadcrumbs";
  
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -14,9 +15,9 @@ export default async function Page() {
     
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
-                Dashboard
-            </h1>
+            <Breadcrumbs breadcrumbs={[
+                { label: 'Dashboard', href: '/dashboard', active: true }
+            ]} />
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
                 <Suspense fallback={<CardsSkeleton />}>
                     <CardWrapper />
